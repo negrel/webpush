027aeba86d66584c49056a48a84d5290c0807088
Alexandre Negrel <alexandre@negrel.dev>
2024-07-13 17:31:18 +0200

    remove dead code

--- static/service-worker.js
+++
@@ -1,9 +0,0 @@
-console.log("Service Worker Loaded...");
-
-self.addEventListener("push", (e) => {
-  const data = e.data.text();
-  console.log("Push Recieved...", data);
-  self.registration.showNotification(data, {
-    body: "Knock Knock",
-  });
-});
