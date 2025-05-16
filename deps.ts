9c7f67c655a3f1d7d12149269a35f96aa1b815b0
Alexandre Negrel <alexandre@negrel.dev>
2025-05-16 15:47:30 +0200

    update deps

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
 export {
   decodeBase64Url,
   encodeBase64Url,
-} from "jsr:@std/encoding@0.224.0/base64url";
+} from "jsr:@std/encoding@1/base64url";
 
-export * as bytes from "jsr:@std/bytes@0.224.0";
+export * as bytes from "jsr:@std/bytes@1";
 
-export * as ece from "jsr:@negrel/http-ece@0.6.0";
+export * as ece from "jsr:@negrel/http-ece@0.7.0";
--- dev_deps.ts
+++ dev_deps.ts
@@ -1,2 +1,2 @@
-export * from "jsr:@std/assert";
+export * from "jsr:@std/assert@1";
 export * as jose from "https://deno.land/x/jose@v5.3.0/index.ts";
