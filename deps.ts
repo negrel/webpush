49d52bcdaf188bdd06bbfc1db936fee978dc09da
Alexandre Negrel <alexandre@negrel.dev>
2025-05-16 15:36:08 +0200

    remove unused imports

--- deps.ts
+++ deps.ts
@@ -4,7 +4,5 @@ export {
 } from "jsr:@std/encoding@0.224.0/base64url";
 
 export * as bytes from "jsr:@std/bytes@0.224.0";
-export { contentType } from "jsr:@std/media-types@0.224.0";
-export { extname } from "jsr:@std/path@0.224.0";
 
 export * as ece from "jsr:@negrel/http-ece@0.6.0";
